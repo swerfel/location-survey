f00fd1b3c0708c3d9fab481660693e667f12e0b7
Sergej Werfel <email@sergej-werfel.de>
2020-12-02 20:55:39 +0100

    server implemented

--- index.js
+++ index.js
@@ -9,8 +9,34 @@ app.use(cors());
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-io.on('connection', (socket) => this._connectionEstablished(socket));
-io.on('error', (error) => this._onError(error));
+io.on('connection', (socket) => {
+	socket.on('new location', location => clietSetLocation(socket, location))
+	socket.emit('locations', flatLocations());
+});
+io.on('error', (error) => {
+	console.log(error);
+});
+
+const locations = {};
+var dirty = false;
+setInterval(sendLocationsToClients, 500);
+
+function flatLocations() {
+	return Object.values(locations);
+}
+
+function clietSetLocation(socket, location) {
+	locations[socket.id] = location;
+	dirty = true;
+	socket.emit('locations', flatLocations())
+}
+
+function sendLocationsToClients(){
+	if (true) {
+		dirty = false;
+		io.emit('locations', flatLocations());
+	}
+}
 
 // Frontend logic
 app.get('/', (req, res) => {
@@ -18,4 +44,12 @@ app.get('/', (req, res) => {
 });
 app.get('/DACH.png', (req, res) => {
 	res.sendFile(__dirname+'/DACH.png');
+});
+
+app.use(express.static(__dirname+'/node_modules/socket.io/client-dist/'))
+
+
+// let's go
+http.listen(PORT, () => {
+  console.log('listening on port: ' + PORT);
 });
